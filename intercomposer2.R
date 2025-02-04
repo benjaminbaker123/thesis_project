77449ccbb45aefd6b9064d93605301f46342077b
Benjamin Baker <bakerb15@mcmaster.ca>
2025-02-03 22:16:02 -0500

    Update intercomposer2.R
    - Fixed key labels by taking out "m" for minor keys and making minor keys lowercase

--- intercomposer2.R
+++ intercomposer2.R
@@ -14,19 +14,30 @@ library(ggsignif)
 # Function to determine significance stars
 get_significance_label <- function(p_value) {
   if (p_value < 0.001) {
-    return("***")
+    return("p < 0.001 ***")
   } else if (p_value < 0.01) {
-    return("**")
+    return("p < 0.01 **")
   } else if (p_value < 0.05) {
-    return("*")
+    return("p < 0.05 *")
   } else {
-    return("NS") # Not Significant
+    return("p > 0.05 NS") # Not Significant
   }
+}
+
+process_shortened_pieceID_name <- function(df) {
+  minor_rows <- grepl("minor", df$Mode)  # Identify rows where mode is "minor"
+  
+  df$shortened_pieceID_name[minor_rows] <- paste0(
+    tolower(substr(df$shortened_pieceID_name[minor_rows], 1, 1)), 
+    substr(df$shortened_pieceID_name[minor_rows], 2, nchar(df$shortened_pieceID_name[minor_rows]))
+  )
+  
+  return(df)
 }
 
 ### Attack Rate ###
 
-analyze_attack_rate <- function(title, composer1, composer2, file_path) {
+analyze_attack_rate <- function(title, composer1, composer2, file_path, show_legend = "y") {
   # Read CSV
   ar <- read.csv(file_path)
   composer = composer1
@@ -53,14 +64,24 @@ analyze_attack_rate <- function(title, composer1, composer2, file_path) {
       pieceID_name = pretty_pieceID(New_Piece_ID, setCode)) -> ar
   
   ar |>
-    mutate(shortened_pieceID_name = str_replace(pieceID_name, " minor$", "m")) |>  # Replace " minor" at the end with "m"
+    mutate(shortened_pieceID_name = str_replace(pieceID_name, " minor$", "")) |>  # Replace " minor" at the end with "m"
     mutate(shortened_pieceID_name = str_replace(shortened_pieceID_name, " Major$", ""))  -> ar
+  
+  ar <- process_shortened_pieceID_name(ar)
   
   # Calculate correlation and p-value
   r <- cor.test(ar$Stimuli, ar$Full.Tracks)$estimate
   p_og <- cor.test(ar$Stimuli, ar$Full.Tracks)$p.value
   adjusted_p <- p.adjust(p_og, "bonferroni", n = 3)
-  sig_label <- get_significance_label(adjusted_p)
+  
+  # Calculate R2 and p-value
+  model <- lm(Stimuli~Full.Tracks, data=ar)
+  
+  #view model summary
+  r2 <- summary(model)$r.squared
+  r2_p_og <- summary(model)$coefficients[2, 4]
+  r2_adjusted_p <- p.adjust(r2_p_og, "bonferroni", n = 3)
+  sig_label <- get_significance_label(r2_adjusted_p)
   
   # Plot with ggplot
   plot <- ggplot(ar, aes(x = Stimuli, y = Full.Tracks, label = shortened_pieceID_name)) +
@@ -75,24 +96,41 @@ analyze_attack_rate <- function(title, composer1, composer2, file_path) {
       legend.title = element_text(size = 16)
     ) +
     geom_abline(color = "gray", linetype = "dashed") +
-    annotate("text", x = 0, y = max(8), 
-             label = paste0("R = ", signif(r, 3), ", p = ", signif(adjusted_p, 3), " ", sig_label), 
+    annotate("text", x = 0, y = max(10), 
+             label = paste0("R² = ", signif(r2, 3), ", ", sig_label), 
              hjust = 0, vjust = 1, size = 6) +
     geom_smooth(method = "lm", color = "black", alpha = 0.2) + # Lighter error bars (alpha set to 0.2)
-    xlim(0, 8) + 
-    ylim(0, 8) +
-    xlab(paste(composer1, "Attack Rate (onsets/second)")) +
-    ylab(paste(composer2, "Attack Rate (onsets/second)")) +
+    xlim(0, 10) + 
+    ylim(0, 10) +
+    xlab(paste("Composer 1 Attack Rate")) +
+    ylab(paste("Composer 2 Attack Rate")) +
     geom_rug() +
     geom_text_repel(aes(colour = Mode), size = 5) +
     scale_colour_manual(values = c("minor" = "#799afd", "Major" = "#a00202")) +
     theme_maple()
+  
+  # Hide the legend if show_legend is "n"
+  if (show_legend == "n") {
+    plot <- plot + theme(legend.position = "none")
+  } else {
+    # Show the legend inside the plot
+    plot <- plot + theme(
+      legend.position = c(0.5, 0.85),
+      legend.background = element_rect(
+        fill = "white",   # Background color
+        color = "black",  # Border color
+        size = 0.5        # Border thickness
+      ),
+      legend.direction = "horizontal"
+    )
+  }
+  
   return(plot)
 }
 
 ### Pitch Height ###
 
-analyze_pitch_height <- function(title, composer1, composer2, file_path) {
+analyze_pitch_height <- function(title, composer1, composer2, file_path, show_legend = "y") {
   # Read CSV
   ph <- read.csv(file_path)
   composer = composer1
@@ -119,14 +157,24 @@ analyze_pitch_height <- function(title, composer1, composer2, file_path) {
       pieceID_name = pretty_pieceID(New_Piece_ID, setCode)) -> ph
   
   ph |>
-    mutate(shortened_pieceID_name = str_replace(pieceID_name, " minor$", "m")) |>  # Replace " minor" at the end with "m"
+    mutate(shortened_pieceID_name = str_replace(pieceID_name, " minor$", "")) |>  # Replace " minor" at the end with "m"
     mutate(shortened_pieceID_name = str_replace(shortened_pieceID_name, " Major$", ""))  -> ph
+  
+  ph <- process_shortened_pieceID_name(ph)
   
   # Calculate correlation and p-value
   r <- cor.test(ph$Stimuli, ph$Full.Tracks)$estimate
   p_og <- cor.test(ph$Stimuli, ph$Full.Tracks)$p.value
   adjusted_p <- p.adjust(p_og, "bonferroni", n = 3)
-  sig_label <- get_significance_label(adjusted_p)
+  
+  # Calculate R2 and p-value
+  model <- lm(Stimuli~Full.Tracks, data=ph)
+  
+  #view model summary
+  r2 <- summary(model)$r.squared
+  r2_p_og <- summary(model)$coefficients[2, 4]
+  r2_adjusted_p <- p.adjust(r2_p_og, "bonferroni", n = 3)
+  sig_label <- get_significance_label(r2_adjusted_p)
   
   # Plot with points
   plot <- ggplot(ph, aes(x = Stimuli, y = Full.Tracks, label = shortened_pieceID_name)) +
@@ -141,25 +189,41 @@ analyze_pitch_height <- function(title, composer1, composer2, file_path) {
       legend.title = element_text(size = 16)
     ) +
     geom_abline(color = "gray", linetype = "dashed") +
-    annotate("text", x = 53, y = 69, 
-             label = paste0("R = ", signif(r, 3), ", p = ", signif(adjusted_p, 3), " ", sig_label), 
+    annotate("text", x = 50, y = 73, 
+             label = paste0("R² = ", signif(r2, 3), ", ", sig_label), 
              hjust = 0, vjust = 1, size = 6) +
     geom_smooth(method = "lm", color = "black", alpha = 0.2) + # Lighter error bars (alpha set to 0.2)
-    xlim(53, 69) + 
-    ylim(53, 69) +
-    xlab(paste(composer1, "Average Pitch Height (out of 88)")) +
-    ylab(paste(composer2, "Average Pitch Height (out of 88)")) +
+    xlim(50, 73) + 
+    ylim(50, 73) +
+    xlab(paste("Composer 1 Average Pitch Height")) +
+    ylab(paste("Composer 2 Average Pitch Height")) +
     geom_rug() +
     geom_text_repel(aes(colour = Mode), size = 5) +
     scale_colour_manual(values = c("minor" = "#799afd", "Major" = "#a00202")) +
     theme_maple()
+  
+  # Hide the legend if show_legend is "n"
+  if (show_legend == "n") {
+    plot <- plot + theme(legend.position = "none")
+  } else {
+    # Show the legend inside the plot
+    plot <- plot + theme(
+      legend.position = c(0.5, 0.1),
+      legend.background = element_rect(
+        fill = "white",   # Background color
+        color = "black",  # Border color
+        size = 0.5        # Border thickness
+      ),
+      legend.direction = "horizontal"
+    )
+  }
   
   return(plot)
 }
 
 ### Mode ###
 
-analyze_mode <- function(title, composer1, composer2, file_path) {
+analyze_mode <- function(title, composer1, composer2, file_path, show_legend = "y") {
   # Read CSV
   mode <- read.csv(file_path)
   composer = composer1
@@ -186,14 +250,24 @@ analyze_mode <- function(title, composer1, composer2, file_path) {
       pieceID_name = pretty_pieceID(New_Piece_ID, setCode)) -> mode
   
   mode |>
-    mutate(shortened_pieceID_name = str_replace(pieceID_name, " minor$", "m")) |>  # Replace " minor" at the end with "m"
+    mutate(shortened_pieceID_name = str_replace(pieceID_name, " minor$", "")) |>  # Replace " minor" at the end with "m"
     mutate(shortened_pieceID_name = str_replace(shortened_pieceID_name, " Major$", ""))  -> mode
+  
+  mode <- process_shortened_pieceID_name(mode)
   
   # Calculate correlation and p-value
   r <- cor.test(mode$Stimuli, mode$Full.Tracks)$estimate
   p_og <- cor.test(mode$Stimuli, mode$Full.Tracks)$p.value
   adjusted_p <- p.adjust(p_og, "bonferroni", n = 3)
-  sig_label <- get_significance_label(adjusted_p)
+  
+  # Calculate R2 and p-value
+  model <- lm(Stimuli~Full.Tracks, data=mode)
+  
+  #view model summary
+  r2 <- summary(model)$r.squared
+  r2_p_og <- summary(model)$coefficients[2, 4]
+  r2_adjusted_p <- p.adjust(r2_p_og, "bonferroni", n = 3)
+  sig_label <- get_significance_label(r2_adjusted_p)
   
   # Plot with points
   plot <- ggplot(mode, aes(x = Stimuli, y = Full.Tracks, label = shortened_pieceID_name)) +
@@ -209,59 +283,84 @@ analyze_mode <- function(title, composer1, composer2, file_path) {
     ) +
     geom_abline(color = "gray", linetype = "dashed") +
     annotate("text", x = -0.5, y = 0.5, 
-             label = paste0("R = ", signif(r, 3), ", p = ", signif(adjusted_p, 3), " ", sig_label), 
+             label = paste0("R² = ", signif(r2, 3), ", ", sig_label), 
              hjust = 0, vjust = 1, size = 6) +
     geom_smooth(method = "lm", color = "black", alpha = 0.2) + # Lighter error bars (alpha set to 0.2)
     xlim(-0.5, 0.5) + 
     ylim(-0.5, 0.5) +
-    xlab(paste(composer1, "Mode Correlation (-1 = Minor, 1 = Major)")) +
-    ylab(paste(composer2, "Mode Correlation (-1 = Minor, 1 = Major)")) +
+    xlab(paste("Composer 1 Mode Correlation")) +
+    ylab(paste("Composer 2 Mode Correlation")) +
     geom_rug() +
     geom_text_repel(aes(colour = Mode), size = 5) +
     scale_colour_manual(values = c("minor" = "#799afd", "Major" = "#a00202")) +
     theme_maple()
   
+  # Hide the legend if show_legend is "n"
+  if (show_legend == "n") {
+    plot <- plot + theme(legend.position = "none")
+  } else {
+    # Show the legend inside the plot
+    plot <- plot + theme(
+      legend.position = c(0.5, 0.1),
+      legend.background = element_rect(
+        fill = "white",   # Background color
+        color = "black",  # Border color
+        size = 0.5        # Border thickness
+      ),
+      legend.direction = "horizontal"
+    )
+  }
+  
   return(plot)
 }
 
-plot1 <- analyze_attack_rate("Scriabin-Shostakovich", "Scriabin", "Shostakovich", "ar_sc_st.csv")
+plot1 <- analyze_attack_rate("Scriabin-Shostakovich", "Scriabin", "Shostakovich", "ar_sc_st.csv", show_legend = "n")
 plot2 <- analyze_attack_rate("Scriabin-Kabalevsky", "Scriabin", "Kabalevsky", "ar_sc_ka.csv")
-plot3 <- analyze_attack_rate("Shostakovich-Kabalevsky", "Shostakovich", "Kabalevsky", "ar_st_ka.csv")
+plot3 <- analyze_attack_rate("Shostakovich-Kabalevsky", "Shostakovich", "Kabalevsky", "ar_st_ka.csv", show_legend = "n")
+
+plot1 <- plot1 + theme(axis.title.x = element_blank())  # Remove x-axis label
+plot2 <- plot2 + theme(axis.title.y = element_blank())  # Remove y-axis label
+plot3 <- plot3 + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank())  # Remove x and y-axis label
 
 attack_rate_plot <- (plot1 + plot2 + plot3) +
-  plot_layout(guides = "collect") & 
-  theme(legend.position = "bottom")
+  plot_layout(guides = "keep")  # Avoid "collect" to keep individual plot legends
 
 # Save the plot with the combined legend and labels
 ggsave("inter_attack_rate.png", attack_rate_plot, 
        unit = "px", 
-       height = 2000, 
+       height = 1750, 
        width = 5000)
 
-plot4 <- analyze_pitch_height("Scriabin-Shostakovich", "Scriabin", "Shostakovich", "ph_sc_st.csv")
+plot4 <- analyze_pitch_height("Scriabin-Shostakovich", "Scriabin", "Shostakovich", "ph_sc_st.csv", show_legend = "n")
 plot5 <- analyze_pitch_height("Scriabin-Kabalevsky", "Scriabin", "Kabalevsky", "ph_sc_ka.csv")
-plot6 <- analyze_pitch_height("Shostakovich-Kabalevsky", "Shostakovich", "Kabalevsky", "ph_st_ka.csv")
+plot6 <- analyze_pitch_height("Shostakovich-Kabalevsky", "Shostakovich", "Kabalevsky", "ph_st_ka.csv", show_legend = "n")
+
+plot4 <- plot4 + theme(axis.title.x = element_blank())  # Remove x-axis label
+plot5 <- plot5 + theme(axis.title.y = element_blank())  # Remove y-axis label
+plot6 <- plot6 + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank())  # Remove x and y-axis label
 
 pitch_height_plot <- (plot4 + plot5 + plot6) +
-  plot_layout(guides = "collect") & 
-  theme(legend.position = "bottom")
+  plot_layout(guides = "keep")  # Avoid "collect" to keep individual plot legends
 
 # Save the plot with the combined legend and labels
 ggsave("inter_pitch_height.png", pitch_height_plot, 
        unit = "px", 
-       height = 2000, 
+       height = 1750, 
        width = 5000)
 
-plot7 <- analyze_mode("Scriabin-Shostakovich", "Scriabin", "Shostakovich", "mode_sc_st.csv")
+plot7 <- analyze_mode("Scriabin-Shostakovich", "Scriabin", "Shostakovich", "mode_sc_st.csv", show_legend = "n")
 plot8 <- analyze_mode("Scriabin-Kabalevsky", "Scriabin", "Kabalevsky", "mode_sc_ka.csv")
-plot9 <- analyze_mode("Shostakovich-Kabalevsky", "Shostakovich", "Kabalevsky", "mode_st_ka.csv")
+plot9 <- analyze_mode("Shostakovich-Kabalevsky", "Shostakovich", "Kabalevsky", "mode_st_ka.csv", show_legend = "n")
+
+plot7 <- plot7 + theme(axis.title.x = element_blank())  # Remove x-axis label
+plot8 <- plot8 + theme(axis.title.y = element_blank())  # Remove y-axis label
+plot9 <- plot9 + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank())  # Remove x and y-axis label
 
 mode_plot <- (plot7 + plot8 + plot9) +
-  plot_layout(guides = "collect") & 
-  theme(legend.position = "bottom")
+  plot_layout(guides = "keep")  # Avoid "collect" to keep individual plot legends
 
 # Save the plot with the combined legend and labels
 ggsave("inter_mode.png", mode_plot, 
        unit = "px", 
-       height = 2000, 
+       height = 1750, 
        width = 5000)
